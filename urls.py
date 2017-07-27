2f100d6e0899b831cc027a0d4173d492e498b151
savana26 <josiah_lim@dlsu.edu.ph>
2017-07-27 15:41:52 +0800

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-
-    url(r'^(?P<username_id>[0-9]+)/$', views.profile, name='profile'),
-]
