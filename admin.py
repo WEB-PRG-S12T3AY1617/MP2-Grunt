b7865a57c59270fcc9701f7161fbfa9d21815aa0
savana26 <josiah_lim@dlsu.edu.ph>
2017-07-27 15:39:44 +0800

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-from .models import Item
-from .models import Tags
-
-admin.site.register(User)
-admin.site.register(Item)
-admin.site.register(Tags)
