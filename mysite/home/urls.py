a5d0e6a3b6ce3c7c8768e475f4bc565d4e56089a
savana26 <josiah_lim@dlsu.edu.ph>
2017-07-27 16:08:44 +0800

    Update urls.py

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    url(r'^(?P<username_id>[0-9]+)/$', views.profile, name='profile'),
 ]
