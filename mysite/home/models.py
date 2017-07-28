1bc3dc824f6062aa96c69b6710d62e9bf6c2effb
savana26 <josiah_lim@dlsu.edu.ph>
2017-07-28 23:05:46 +0800

    Update models.py

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 from django.utils import timezone
+
 
 class User(models.Model):
     username = models.CharField(max_length=21)
@@ -7,26 +9,33 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     degree = models.CharField(max_length=100, blank=True, help_text='Leave blank if personnel/faculty')
     office = models.CharField(max_length=100, blank=True, help_text='Leave blank if student')
-    
+
+    def get_absolute_url(self):
+        return reverse('home:home', kwargs={'pk': self.pk})
+
     def __str__(self):
         return self.username
 
+
 class Item(models.Model):
-    
     userName = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=300)
+    name = models.CharField(primary_key=True, max_length=300)
     photo = models.FileField()
     quantity = models.PositiveIntegerField(default=1)
     condition = models.CharField(max_length=200)
     itemType = models.CharField(max_length=100, help_text='Office Use/Academic Use')
     courseName = models.CharField(max_length=100, blank=True, help_text='Leave blank if Office Use')
     pub_date = models.DateTimeField('date published')
-    
+
+    def get_absolute_url(self):
+        return reverse('home:home', kwargs={'pk': self.pk})
+
     def __str__(self):
         return self.name
-    
+
+
 class Tags(models.Model):
-    itemName = models.ForeignKey(Item, on_delete=models.CASCADE)
+    name = models.ForeignKey(Item, on_delete=models.CASCADE)
     tag = models.CharField(max_length=100)
     
     def __str__(self):
