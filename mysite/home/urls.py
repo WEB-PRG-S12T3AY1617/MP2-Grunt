14ff1470d0601fe4bce004660c8ebb4baba2ad85
savana26 <josiah_lim@dlsu.edu.ph>
2017-07-28 23:06:51 +0800

    Update urls.py

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -1,8 +1,27 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
 
     url(r'^(?P<username_id>[0-9]+)/$', views.profile, name='profile'),
+
+    # add an item
+    url(r'^item/add/$', views.ItemCreate.as_view(success_url="/home/"), name='item-add'),
+
+    # update an item
+    url(r'item/(?P<pk>[0-9]+)/$', views.ItemUpdate.as_view(), name='item-update'),
+
 ]
+
+
+
+
+
+
+
+
