61f4fcae640efca4ed8dfa798c04e3d7e5517d46
legaspielaine <elaineelegaspi@gmail.com>
2017-07-28 15:56:47 +0800

    Update views.py

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import User, Item, Tags
 
 def index(request):
@@ -10,11 +10,13 @@ def index(request):
               }
     return render(request, 'home/home.html', context)
 
-def profile(request, username_id):
-    # return HttpResponse("<h2>This is the profile page of user: " + str(username_id) + "</h2>")
-    try:
-        user = User.objects.get(pk=username_id)
-    except User.DoesNotExist:
-        raise Http404("User does not exist")
-    return render(request, 'home/profile.html', {'user': user})
 
+def profile(request, username_id):
+    user = get_object_or_404(User, pk=username_id)
+    latest_item_list = Item.objects.order_by('-pub_date')
+    latest_tag_list = Tags.objects.all()
+    context = {'user':user,
+               'latest_item_list': latest_item_list,
+               'latest_tag_list': latest_tag_list,
+              }
+    return render(request, 'home/profile.html', context)
